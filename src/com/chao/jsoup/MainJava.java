c0ae539d21f569c08cf249aedde14d96f1c42163
ZhangZhiChao <zhangzhichaolove@vip.qq.com>
2017-10-16 09:44:15 +0800

    update pool

--- src/com/chao/jsoup/MainJava.java
+++ src/com/chao/jsoup/MainJava.java
@@ -26,6 +26,7 @@ public class MainJava {
         HibernateUtils.openSession();
         //saveSatin(1);
         //saveBaiSiBuDeJieApi(1, null);
+        start();
     }
 
     public static void start() {
