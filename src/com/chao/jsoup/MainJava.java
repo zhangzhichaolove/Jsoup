562a08aafd76921f26487c7fa35d3cbdc0fd3cdd
ZhangZhiChao <zhangzhichaolove@vip.qq.com>
2017-10-16 09:26:53 +0800

    update pool

--- src/com/chao/jsoup/MainJava.java
+++ src/com/chao/jsoup/MainJava.java
@@ -3,6 +3,7 @@ package com.chao.jsoup;
 import com.chao.jsoup.model.BuDeJieContent;
 import com.chao.jsoup.model.BuDeJieModel;
 import com.chao.jsoup.model.SatinModel;
+import com.chao.jsoup.util.ExecutorServiceUtils;
 import com.chao.jsoup.util.HibernateUtils;
 import com.google.gson.Gson;
 import org.hibernate.Criteria;
@@ -19,11 +20,27 @@ import org.jsoup.select.Elements;
  */
 public class MainJava {
     private static Gson gson = new Gson();
+    public static boolean startRun = false;
 
     public static void main(String[] args) {
         HibernateUtils.openSession();
         //saveSatin(1);
-        saveBaiSiBuDeJieApi(1, null);
+        //saveBaiSiBuDeJieApi(1, null);
+    }
+
+    public static void start() {
+        startRun = true;
+        HibernateUtils.openSession();
+        ExecutorServiceUtils.getInstance().execute(new Runnable() {
+            @Override
+            public void run() {
+                saveSatin(1);
+            }
+        });
+    }
+
+    public static void stop() {
+        startRun = false;
     }
 
     public static void saveSatin(int page) {
@@ -34,6 +51,7 @@ public class MainJava {
             //Elements list = doc.getElementsByClass("j-list-user");
             Elements elements = doc.getElementsByClass("j-r-list-c-desc");
             //doc.getElementsByClass("j-r-list-c-desc").get(0).getElementsByTag("a").get(0).getAllElements().get(0).text();
+            int identical = 0;
             for (int i = 0; i < elements.size(); i++) {
                 Element element = elements.get(i);
                 String text = element.getElementsByTag("a").get(0).getAllElements().get(0).text();
@@ -48,16 +66,27 @@ public class MainJava {
                     model.setContent(text);
                     session.save(model);
                     transaction.commit();
+                } else {
+                    identical = identical + 1;
                 }
                 session.close();
                 System.out.println(text);
+                if (identical >= elements.size()) {
+                    System.out.println("重复数据超过限制，爬取其他接口。");
+                    saveBaiSiBuDeJieApi(1, null);
+                }
             }
             Elements title = doc.getElementsByTag("title");
             System.out.println(title.text());
-            saveSatin(page + 1);
+            if (startRun) {
+                saveSatin(page + 1);
+            } else {
+                System.out.println("服务停止!");
+            }
         } catch (CommonException e) {
             e.printStackTrace();
             System.out.println("请求失败了");
+            saveBaiSiBuDeJieApi(1, null);
         }
     }
 
@@ -68,6 +97,7 @@ public class MainJava {
         BuDeJieModel model = gson.fromJson(json, BuDeJieModel.class);
         if (model != null) {
             maxtime = model.getInfo().getMaxtime();
+            int identical = 0;
             for (int i = 0; i < model.getList().size(); i++) {
                 BuDeJieContent content = model.getList().get(i);
                 System.out.println(content.getText());
@@ -80,13 +110,24 @@ public class MainJava {
                     //content.setId(null);
                     session.save(content);
                     transaction.commit();
+                } else {
+                    identical = identical + 1;
                 }
                 session.close();
             }
             page = page + 1;
-            saveBaiSiBuDeJieApi(page, maxtime);
+            if (identical >= model.getList().size()) {
+                System.out.println("重复数据超过限制，爬取其他接口。");
+                saveSatin(1);
+            }
+            if (startRun) {
+                saveBaiSiBuDeJieApi(page, maxtime);
+            } else {
+                System.out.println("服务停止!");
+            }
         } else {
             System.out.println("保存完毕!");
+            saveSatin(1);
         }
 
     }
